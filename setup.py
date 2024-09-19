9ed8d37356204b646b5fc19a49e20907ed43c196
krutto89 <sitb01-028692022@student.mmust.ac.ke>
2024-09-19 21:24:34 +0300

    another code update

--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
-
-def get_requirements(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
-   
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.3"
+__version__ = "0.0.2"
 REPO_NAME = "MongoConnect"
-PKG_NAME= "MongoConnectdb"
+PKG_NAME= "mongo_connectdb"
 AUTHOR_USER_NAME = "krutto89"
 AUTHOR_EMAIL = "sitb01-028692022@student.mmust.ac.ke"
 
@@ -38,6 +25,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("requirements_dev.txt"),
-    
-)
+    )
+
+
